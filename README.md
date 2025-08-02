62df787ee990df073f8512dbb6c387123622b76c
Krzysztof Lenarczyk <krzysztoflenarczyk06@gmail.com>
2025-08-02 12:25:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Twitter Clone
+
+A Full-Stack Twitter Clone.
+
+---
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Twitter Clone</h1>
+      <p className="mt-4 text-lg">Initial Commit.</p>
+    </div>
+  );
+}
