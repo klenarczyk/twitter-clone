f8d1fca47541b701fbeb5ddba1c9146bf9522f71
Krzysztof Lenarczyk <krzysztoflenarczyk06@gmail.com>
2025-09-10 17:47:23 +0200

    style(profile): change the profile page UI

--- frontend/src/app/(main)/u/[handle]/page.tsx
+++ frontend/src/app/(main)/u/[handle]/page.tsx
@@ -3,50 +3,90 @@
 import Image from "next/image";
 import { useParams } from "next/navigation";
 
-import Feed from "@/features/post/components/InfinitePostList";
+import InfinitePostList from "@/features/post/components/InfinitePostList";
 import { useProfile } from "@/features/profile/hooks/useProfile";
 import { getProfileImage } from "@/features/profile/utils/getProfileImage";
+import { ChevronLeft } from "lucide-react";
 
 export default function ProfilePage() {
 	const params = useParams();
 	const { profile, loading } = useProfile(params?.handle as string);
 	const imageUrl = getProfileImage(profile?.imageUrl, loading);
 
-	if (loading) return <div className="text-white">Loading...</div>;
+	if (loading) {
+		return (
+			<div className="flex justify-center items-center h-64">
+				<div className="animate-spin rounded-full h-10 w-10 border-4 border-zinc-600 border-t-transparent"></div>
+			</div>
+		);
+	}
 
-	if (!profile) return <div className="text-white">Profile not found</div>;
+	if (!profile) {
+		return (
+			<div className="flex justify-center items-center h-64">
+				<p className="text-zinc-400">Profile not found</p>
+			</div>
+		);
+	}
 
 	return (
-		<div className="w-clamped">
-			<div className="flex items-start justify-center gap-6 border-b border-[var(--color-500)] pb-4 mb-6">
-				<div className="flex justify-start w-32 h-32 relative">
-					{loading || !imageUrl ? (
-						<div className="w-10 h-10 border-4 border-t-4 border-mono-600 border-t-mono-200 rounded-full animate-spin" />
-					) : (
-						<Image
-							src={imageUrl}
-							alt="Profile picture"
-							width={500}
-							height={500}
-							priority={true}
-							className="rounded-full object-cover cursor-pointer"
-						/>
-					)}
-				</div>
-				<div className="flex-1">
-					<p className="font-bold text-xl text-mono-100">{profile.fullName}</p>
-					<p className="text-mono-500 font-semibold">@{profile.handle}</p>
-					<div className="flex gap-6 mt-3 text-mono-300 font-semibold">
-						<span>X Followers</span>
-						<span>Y Following</span>
-						<span>Z Posts</span>
+		<div className="md:space-y-4 mb-14 md:mb-8 relative w-full max-w-2xl mx-auto">
+			<div className="hidden md:flex sticky top-14 md:top-4 z-10 items-center px-4 py-1 mb-8">
+				<button
+					onClick={() => window.history.back()}
+					className="p-2 rounded-full transition cursor-pointer"
+				>
+					<ChevronLeft className="text-white hover:text-zinc-400 transition-colors" />
+				</button>
+
+				<button
+					onClick={(e) => {
+						e.preventDefault();
+						window.scrollTo({ top: 0, behavior: "smooth" });
+					}}
+					className="flex-1 text-center cursor-pointer"
+				>
+					<h1 className="text-lg font-semibold text-white">{profile.handle}</h1>
+				</button>
+
+				<div className="w-9" />
+			</div>
+
+			{/* Profile Header */}
+			<div className="md:bg-zinc-900 rounded-2xl shadow-sm md:border md:border-zinc-800 pt-6 pb-4 px-6">
+				<div className="flex items-start gap-6">
+					<div className="w-24 h-24 relative">
+						{!imageUrl ? (
+							<div className="w-10 h-10 border-4 border-t-4 border-zinc-600 border-t-zinc-200 rounded-full animate-spin mx-auto" />
+						) : (
+							<Image
+								src={imageUrl}
+								alt="Profile picture"
+								width={96}
+								height={96}
+								className="rounded-full object-cover"
+							/>
+						)}
 					</div>
-					<p className="my-4 text-mono-100">{profile.bio}</p>
+
+					<div className="flex-1">
+						<p className="font-bold text-xl text-white">{profile.fullName}</p>
+						<p className="text-zinc-400 font-medium">@{profile.handle}</p>
+						<div className="flex gap-6 mt-3 text-zinc-300 font-medium">
+							<span>X Followers</span>
+							<span>Y Following</span>
+							<span>Z Posts</span>
+						</div>
+						<p className="mt-4 text-zinc-200">{profile.bio}</p>
+					</div>
 				</div>
 			</div>
 
-			<div className="flex justify-center">
-				<Feed authorId={profile.id} initialPageSize={8} className="w-full" />
+			{/* Posts */}
+			<div className="md:bg-zinc-900 rounded-2xl shadow-sm md:border md:border-zinc-800 md:mt-4 pt-2">
+				<main className="w-full max-w-2xl mx-auto">
+					<InfinitePostList authorId={profile.id} initialPageSize={8} />
+				</main>
 			</div>
 		</div>
 	);
