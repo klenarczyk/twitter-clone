fd2b8d8d978aec1f6ae672734e49869150840965
Krzysztof Lenarczyk <149272818+klenarczyk@users.noreply.github.com>
2025-09-13 16:24:41 +0200

    chore: update README

--- backend/src/main/java/com/klenarczyk/backend/LoopBackendApplication.java
+++ backend/src/main/java/com/klenarczyk/backend/LoopBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TwitterCloneBackendApplication {
+public class LoopBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TwitterCloneBackendApplication.class, args);
+		SpringApplication.run(LoopBackendApplication.class, args);
 	}
 
 }
